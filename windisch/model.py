65c510bb5bf0ef56b513bcc1b4b2311a4948e243
romainsacchi <r_s@me.com>
2024-10-14 11:50:12 +0200

    ADDED A COMMENT

--- windisch/model.py
+++ windisch/model.py
@@ -454,6 +454,8 @@ class WindTurbineModel:
             self["power"], [30, 150, 600, 800, 2000], [150, 300, 862, 1112, 3946]
         )
 
+        # THIS IS A COMMENT
+
     def __set_foundation_mass(self):
         """
         Define mass of foundation.
