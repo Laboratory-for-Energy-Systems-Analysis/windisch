c71b2b11d5b7e39c42a5b892fabef158ff495da7
romainsacchi <romain@Romains-iMac.psi.ch>
2025-02-13 15:04:53 +0100

    Add inventories relating to assembly and maintenance

--- windisch/inventory.py
+++ windisch/inventory.py
@@ -806,6 +806,115 @@ class Inventory:
             )
         )
 
+        # assembly electricity (0.5 kWh per kg of wind turbine)
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("wind turbine production, ")
+        ]
+
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market group for electricity, medium voltage",)
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter="assembly electricity",
+        ) * -1
+
+        # transport to assembly site and transport to use site
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for transport, freight, lorry, unspecified",)
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter=[
+                "transport to assembly",
+                "installation transport, by truck",
+            ]
+        ).sum(dim="parameter") * -1
+
+        self.A[
+        :,
+        self.find_input_indices(
+            ("market for transport, freight train",)
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter="installation transport, by rail",
+        ) * -1
+
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for transport, freight, sea, container ship",)
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter="installation transport, by sea",
+        ) * -1
+
+        # access road construction
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("wind turbine production, ")
+            and "onshore" in i[0]
+        ]
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for road",)
+        ),
+        input_indices,
+        ] = (
+                (
+                    self.array.sel(
+                        parameter="access road",
+                        combined_dim=[
+                            d
+                            for d in self.array.coords["combined_dim"].values
+                            if "onshore" in d
+                        ],
+                    )
+                    * self.array.sel(
+                        parameter="lifetime",
+                        combined_dim=[
+                            d
+                            for d in self.array.coords["combined_dim"].values
+                            if "onshore" in d
+                        ],
+                    )
+                )
+            / self.array.sel(
+                    parameter="turbines per farm",
+                    combined_dim=[
+                        d
+                        for d in self.array.coords["combined_dim"].values
+                        if "onshore" in d
+                    ],
+                )
+            ) * -1
+
+        # installation energy
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("wind turbine production, ")
+        ]
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for diesel, burned in building machine",)
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter="installation energy",
+        ) * -1
+
         # maintenance
         input_indices = [
             j
@@ -830,6 +939,104 @@ class Inventory:
             )
             * -1
         )
+
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("electricity production, wind, ")
+            and "onshore" in i[0]
+        ]
+
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for transport, passenger car",)),
+        input_indices,
+        ] = self.array.sel(
+            parameter="maintenance transport",
+            combined_dim=[
+                d
+                for d in self.array.coords["combined_dim"].values
+                if "onshore" in d
+            ],
+        ) / self.array.sel(
+                parameter="lifetime electricity production",
+            ) * -1
+
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("electricity production, wind, ")
+               and "offshore" in i[0]
+        ]
+
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for transport, freight, sea, ferry",)),
+        input_indices,
+        ] = self.array.sel(
+            parameter="maintenance transport",
+            combined_dim=[
+                d
+                for d in self.array.coords["combined_dim"].values
+                if "offshore" in d
+            ],
+        ) / self.array.sel(
+                parameter="lifetime electricity production",
+            ) * -1
+
+
+        # onshore foundation
+
+        # offshore foundation - steel monopile
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("wind turbine production, ")
+               and "offshore" in i[0]
+        ]
+
+
+        self.A[
+        :,
+        self.find_input_indices(
+            ("market for steel, low-alloyed, hot rolled",)
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter="pile mass",
+            combined_dim=[
+                d
+                for d in self.array.coords["combined_dim"].values
+                if "offshore" in d
+            ],
+        ) * -1
+
+        # offshore foundation - grout
+        input_indices = [
+            j
+            for i, j in self.inputs.items()
+            if i[0].startswith("wind turbine production, ")
+            and "offshore" in i[0]
+        ]
+
+        self.A[
+        :,
+        self.find_input_indices(
+            (f"market for concrete, normal strength",), excludes=("RoW",), excludes_in=1
+        ),
+        input_indices,
+        ] = self.array.sel(
+            parameter="grout volume",
+            combined_dim=[
+                d
+                for d in self.array.coords["combined_dim"].values
+                if "offshore" in d
+            ],
+        ) * -1
+
+
 
     def add_wind_turbines_to_electricity_production_dataset(self):
         self.A[
